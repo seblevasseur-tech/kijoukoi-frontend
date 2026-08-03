b85f77708014e9323695f6addb5eb51d4e8b182e
Gamer <seb.levasseur@proton.me>
2026-08-03 16:28:56 +0200

    1st components hello world

---
+++ src/app/player-dashboard/player-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-dashboard.component.html',
+  styleUrl: './player-dashboard.component.scss'
+})
+export class PlayerDashboardComponent {
+
+}
